f6078c22d1172eb34937bc92366784c3ec817d41
faith-kerry <kerubofaith944@gmail.com>
2025-09-17 09:44:54 +0300

    Completed the Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2; // 31 * 2 = 62
+
+// Random integer greater than 0
+let random = Math.ceil(Math.random() * 10); 
+// multiplies by 10 so you get 1–10, then ceil ensures it's never 0
+
+// Modulus (remainder)
+let num3 = 28;
+let num4 = 6;
+
+let mod = num3 % num4; // 28 % 6 = 4
+
+// Maximum value
+let max = Math.max(5, 10, 15, 20); // returns 20
+
